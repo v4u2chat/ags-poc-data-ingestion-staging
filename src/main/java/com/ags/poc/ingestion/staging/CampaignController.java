746a7d1d30e70c24c03237987975ef737719e42e
Rajesh <v4u2chat@gmail.com>
2021-09-22 12:42:09 +0530

    Added changes to deploy as WAR into Tomcat

--- src/main/java/com/ags/poc/ingestion/staging/CampaignController.java
+++ src/main/java/com/ags/poc/ingestion/staging/CampaignController.java
@@ -1,14 +1,29 @@
 package com.ags.poc.ingestion.staging;
 
+import java.util.Date;
+
+import com.ags.poc.ingestion.chroniclemap.CampaignDataInfo;
+import com.ags.poc.ingestion.chroniclemap.CampaignService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CampaignController {
 
-	@GetMapping("/")
+	@Autowired
+	private CampaignService campaignService;
+
+	@GetMapping("/hello")
 	public String hello() {
-		return "Greetings from Spring Boot!";
+		return "Greetings from Spring Boot!\t"+new Date();
+	}
+
+	@GetMapping("/nbr")
+	public CampaignDataInfo nbr(@RequestParam(value = "nbr", defaultValue = "-") String nbr) {
+		return campaignService.getCampaignDataInfo(nbr);
 	}
 
 }
--- src/main/java/com/ags/poc/ingestion/staging/DataIngestionStagingApplication.java
+++ src/main/java/com/ags/poc/ingestion/staging/DataIngestionStagingApplication.java
@@ -1,18 +1,15 @@
 package com.ags.poc.ingestion.staging;
 
-import java.io.File;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
-
-import net.openhft.chronicle.map.ChronicleMap;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableWebMvc
 @ComponentScan(basePackages = "com.ags.poc.ingestion")
-public class DataIngestionStagingApplication {
-
-
+public class DataIngestionStagingApplication extends SpringBootServletInitializer{
 	public static void main(String[] args) throws Exception{
 
 		SpringApplication.run(DataIngestionStagingApplication.class, args);
